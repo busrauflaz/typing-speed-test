3916eb2c249e426a73baaca69bcdf6abd31cd52e
Nazan Ertekin <n.ertekin2025@gtu.edu.tr>
2026-02-25 00:13:02 +0300

    ucuncu

---
+++ typing-speed-test-react/src/Header.jsx
@@ -0,0 +1,28 @@
+function Header(){
+
+    return(
+        <>
+          <section id="top-header">
+                <img src="src/images/logo-large.svg" />
+                <div id="personal-best">
+                    <img src="src/images/icon-personal-best.svg" />
+                    <h3>Personal Best: </h3>
+                </div>
+          </section>
+
+          <section id="game-info">
+            <div id="header-bottom-left">
+
+            </div>
+
+            <div id="header-bottom-right">
+                
+            </div>
+          </section>
+        </>
+    )
+
+
+}
+
+export default Header
